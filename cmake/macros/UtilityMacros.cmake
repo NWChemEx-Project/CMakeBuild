ea1cd2526d935faee8196eb8c6fb99501c4b8e7c
ryanmrichard <ryanmrichard1@gmail.com>
2017-12-13 17:12:25 -0600

    Generalizes make_full_path fxn, adds test, and fixes #17

--- cmake/macros/UtilityMacros.cmake
+++ cmake/macros/UtilityMacros.cmake
@@ -15,12 +15,26 @@
 #              for. This is NOT the files themselves, i.e. whatever you provide
 #              must dereference to the list
 #
-function(make_full_paths __list)
-    set(__prefix "${CMAKE_CURRENT_LIST_DIR}")
+function(prefix_paths __prefix __list)
     foreach(__file ${${__list}})
         list(APPEND __temp_list ${__prefix}/${__file})
     endforeach()
     set(${__list} ${__temp_list} PARENT_SCOPE)
+endfunction()
+
+#
+# Given a variable that contains a list of files (paths relative to the
+# directory from which this macro is invoked), this macro will inplace (i.e.
+# overwrite the input variable) convert the relative paths to full paths.
+#
+# Syntax: make_full_paths(<list>)
+#     - list : A variable containing the list of files you want the full paths
+#              for. This is NOT the files themselves, i.e. whatever you provide
+#              must dereference to the list
+#
+function(make_full_paths __list)
+    set(__prefix "${CMAKE_CURRENT_LIST_DIR}")
+    prefix_paths(${__prefix} ${__list})
 endfunction()
 
 #
