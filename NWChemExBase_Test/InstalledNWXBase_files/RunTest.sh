5cc81f25c152ae12303b45a26bcd3407f499c160
ryanmrichard <ryanmrichard1@gmail.com>
2017-12-26 14:59:23 -0600

    LAPACKE works

--- NWChemExBase_Test/InstalledNWXBase_files/RunTest.sh
+++ NWChemExBase_Test/InstalledNWXBase_files/RunTest.sh
@@ -7,11 +7,11 @@ cd InstalledNWXBase_files
 dummy_repo=$(pwd)/DummyRepo_files
 
 #Build and install dummy repo
-${binary_dir}/RunNWChemExBaseTest.sh DummyRepo ${nwx_path} ${dummy_repo} cmake
+${binary_dir}/RunNWChemExBaseTest.sh DummyRepo ${nwx_path} ${dummy_repo} ${1}
 cd DummyRepo_repo_test/build
 VERBOSE=1 make install
 
 #Try building the test
 my_files=${binary_dir}/InstalledNWXBase_files
 cd ${binary_dir}
-./RunNWChemExBaseTest.sh InstalledNWXBase ${nwx_path} ${my_files} cmake
+./RunNWChemExBaseTest.sh InstalledNWXBase ${nwx_path} ${my_files} ${1}
