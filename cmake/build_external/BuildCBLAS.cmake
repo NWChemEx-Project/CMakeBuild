6dda11f44120d25611df6fb4e8cf41384eddb898
ryanmrichard <ryanmrichard1@gmail.com>
2018-04-09 12:28:05 -0500

    Switches from variables to targets to fix issue 66

--- cmake/build_external/BuildCBLAS.cmake
+++ cmake/build_external/BuildCBLAS.cmake
@@ -3,7 +3,8 @@
 # installation. To do this we use a mock superbuild in case we need to build
 # BLAS for the user.
 #
-find_or_build_dependency(BLAS _was_Found)
+find_or_build_dependency(BLAS)
+package_dependency(BLAS DEPENDENCY_PATHS)
 enable_language(C Fortran)
 
 
@@ -15,5 +16,6 @@ ExternalProject_Add(CBLAS_External
         INSTALL_COMMAND $(MAKE) DESTDIR=${STAGE_DIR}
         CMAKE_CACHE_ARGS ${CORE_CMAKE_LISTS}
                          ${CORE_CMAKE_STRINGS}
+                         ${DEPENDENCY_PATHS}
         )
 add_dependencies(CBLAS_External BLAS_External)
