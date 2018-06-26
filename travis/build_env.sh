0ac7ba258972ceddd4b3d85c1c71f9b28068d0b9
Hubertus van Dam <hvandam@bnl.gov>
2018-06-26 14:27:20 -0400

    HvD: Still not getting g++ 7.

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo add-apt-repository universe && sudo apt update
-sudo apt-get -y install gcc-7 python3.6-dev cmake openmpi-bin libopenmpi-dev make
+#sudo apt-get remove g++
+sudo apt-get -y install g++-7 gcc-7 python3.6-dev cmake openmpi-bin libopenmpi-dev make
