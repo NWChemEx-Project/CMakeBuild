7d72ac1c086172ed3bd2364d50c7ed39e4747790
Hubertus van Dam <hvandam@bnl.gov>
2018-06-26 14:15:32 -0400

    HvD: Maybe python3.6-dev?

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo add-apt-repository universe && sudo apt update
-sudo apt-get -y install gcc-7 python3 cmake openmpi-bin libopenmpi-dev make
+sudo apt-get -y install gcc-7 python3.6-dev cmake openmpi-bin libopenmpi-dev make
