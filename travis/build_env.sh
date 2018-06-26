9b603b8c847d2ce0aec36d463def78fa967c29b2
Hubertus van Dam <hvandam@bnl.gov>
2018-06-25 22:40:09 -0400

    HvD: Install python3.6.

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo add-apt-repository universe && sudo apt update
-sudo apt-get -y install python-dev cmake openmpi-bin libopenmpi-dev make
+sudo apt-get -y install python3.6 cmake openmpi-bin libopenmpi-dev make
