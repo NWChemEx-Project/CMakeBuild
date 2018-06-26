91411471c398be0d5be30435766767934ceae97c
Hubertus van Dam <hvandam@bnl.gov>
2018-06-26 15:59:39 -0400

    HvD: Using update-alternatives now...

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo add-apt-repository universe && sudo apt update
-sudo apt-get remove g++
 sudo apt-get -y install g++-7 gcc-7 python3.6-dev cmake openmpi-bin libopenmpi-dev make
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 90
